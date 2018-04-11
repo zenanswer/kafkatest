cc1ef6145a6a89c4cf13f61841fd7c2e1e63bd0c
xuechenwang <xuechenwang@cienet.com.cn>
2018-04-11 16:09:32 +0800

    Add sample py code

---
+++ producerconsumer/consumer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import logging
+
+
+KAC_logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    import json
+
+    from kafka import KafkaConsumer
+    from kafka.errors import KafkaError
+
+    import util
+
+    util.enable_default_log()
+
+    # To consume latest messages and auto-commit offsets
+    consumer = KafkaConsumer(
+        'my-topic',
+        group_id='my-group',
+        bootstrap_servers=['127.0.0.1:9092'],
+        consumer_timeout_ms=1000,
+        value_deserializer=lambda m: json.loads(m.decode('utf8')))
+
+    try:
+        for message in consumer:
+            # message value and key are raw bytes -- decode if necessary!
+            # e.g., for unicode: `message.value.decode('utf-8')`
+            KAC_logger.info(
+                ">>> %s:%d:%d: key=%s value=%s"
+                % (message.topic, message.partition,
+                    message.offset, message.key,
+                    message.value))
+
+        consumer.close()
+    except KafkaError:
+        # Decide what to do if produce request failed...
+        KAC_logger.exception()
+        pass
---
+++ producerconsumer/producer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import logging
+
+
+KAP_logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    import json
+
+    from kafka import KafkaProducer
+    from kafka.errors import KafkaError
+
+    import util
+
+    util.enable_default_log()
+
+    producer = KafkaProducer(
+        bootstrap_servers=['127.0.0.1:9092'],
+        value_serializer=lambda m: json.dumps(m).encode('utf8'))
+
+    def on_send_success(record_metadata):
+        # print('%'*20)
+        # print(repr(record_metadata))
+        KAP_logger.info(
+            "<<< %s:%d:%d"
+            % (record_metadata.topic, record_metadata.partition,
+                record_metadata.offset))
+
+    def on_send_error(excp):
+        KAP_logger.error('I am an errback', exc_info=excp)
+        # handle exception
+
+    try:
+        for x in range(0, 4):
+            # produce asynchronously with callbacks
+            producer.send(
+                'my-topic',
+                {'key-%s' % str(x): 'value-%s' % str(x)},
+                key=b'my-key')\
+                .add_callback(on_send_success).add_errback(on_send_error)
+
+        # block until all async messages are sent
+        producer.flush()
+
+        producer.close()
+    except KafkaError:
+        # Decide what to do if produce request failed...
+        KAP_logger.exception()
+        pass
